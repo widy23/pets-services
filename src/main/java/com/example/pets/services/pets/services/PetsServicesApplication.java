1105d5442bee8e33d1ca302e4bfb2938af4196fd
Widmaer M Antoine <widmaer.antoine@bancogalicia.com.ar>
2024-03-24 21:38:43 -0300

    Initial commit

---
+++ src/main/java/com/example/pets/services/pets/services/PetsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.pets.services.pets.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsServicesApplication.class, args);
+	}
+
+}
